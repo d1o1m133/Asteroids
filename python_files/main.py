7e5d9d708fd63adb7f6133e9ba439252fcbd268f
d1o1m133 <d.addonizio@yahoo.com>
2025-04-04 09:44:17 -0700

    Add player ship with triangle drawing functionality

--- python_files/main.py
+++ python_files/main.py
@@ -1,10 +1,21 @@
 import pygame
+from player import Player
 from constants import *
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(F"Screen height: {SCREEN_HEIGHT}")
+
 
     game_loop = True
     while game_loop == True:
@@ -13,14 +24,16 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-    screen.fill("black")
-    pygame.display.flip()
+        screen.fill("black")
+
+        player.draw(screen) #draw the player
+
+        pygame.display.flip()
+        elapsed_time = clock.tick(60) 
+        dt = elapsed_time / 1000
 
 
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(F"Screen height: {SCREEN_HEIGHT}")
-
+    
 
 
 
